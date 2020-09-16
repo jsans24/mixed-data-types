c462c4f9b4dbfae8672e22bd99a26e11803f2fe8
jsans24 <jsans24@gmail.com>
2020-09-16 08:34:48 -0700

    Commit 1 - Data Types

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+// ---- DATA TYPES ---- //
+    // ----- 1. A light switch that can be either on or off.
+    
+    // Booleans -  lightSwitchOn = true || false;
+    
+    // ----- 2. A user's email address.
+    
+    // String - let userEmail = 'anon@email.com';
+    
+    // ----- 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    
+    // Object const spaceship = {
+    //     hull: hard,
+    //     laserBeams: hot,
+    //     tractorBeams: pull,
+    //     warpDrive: fast,
+    // };
+    
+    // ----- 4. A list of student names from our class.
+    
+    // Array - const studentList = ['John Doe','Jane Doe','Craig Duke'];
+
+    // ----- 5. A list of student names from our class, each with a location.
+    
+    // Array - const studentList2 = [['john doe', 'San Francisco'], ['Jane Doe', 'Miami'], ['Craig Duke', 'Los Angeles']];
+    
+    // ----- 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    // Array & Object - const studentList3 = [
+    //     {name: 'John Doe',
+    //     location: 'San Francisco',
+    //     favShow: 'Scrubs'},
+    //     {name: 'Jane Doe',
+    //     location: 'Miami',
+    //     favShow: '90210'},
+    //     {name: 'Craig Duke',
+    //     location: 'Los Angeles',
+    //     favShow: 'The Office'},
+    // ];
